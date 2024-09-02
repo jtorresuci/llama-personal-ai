563fd4956617a3ea155d3bf519d5b3c6a9d1543a
Joe Torres <jtorresocc@gmail.com>
2024-09-01 21:26:10 -0700

    Added AI_MODEL .env variable for choosing a model.

--- src/model_setup.py
+++ src/model_setup.py
@@ -1,8 +1,15 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def setup_llama_model():
-    model_name = "meta-llama/Meta-Llama-3-8B"
+    model_name = os.getenv("AI_MODEL")
+    
+    if not model_name:
+        raise ValueError("AI_MODEL not set in .env file")
     
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
